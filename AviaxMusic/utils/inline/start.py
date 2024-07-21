1d9e4f0fac155fe1dbbdfed0c0bf29d84c337f3d
SACHINxHINDU <sachinxxsanatani@gmail.com>
2024-07-21 20:25:28 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -17,24 +17,21 @@ def start_panel(_):
 
 
 def private_panel(_):
-    [
+    buttons = [
+        [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        [
-            InlineKeyboardButton(
-                text="• ʙσᴛ ꜱᴛʌᴛꭎꜱ ʌηᴅ ϻσʀɛ ʙσᴛꜱ •", url=f"https://t.me/All_SANATANI_BOT/324"
-            )
-        ],
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_11"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
