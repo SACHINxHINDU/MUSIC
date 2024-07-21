1f1059e758db619b63aa66b46f595325d4b6db80
SACHINxHINDU <sachinxxsanatani@gmail.com>
2024-07-21 20:23:53 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -53,36 +53,35 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "—————————◉"
     buttons = [
-                [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/HIMANSHI_MUSIC_BOT?startgroup=true",)
-        ],
-        [
-         InlineKeyboardButton(text="• ꭎ֟፝᷍ᴘᴅʌᴛɛ •", url=f"https://t.me/ALL_SANATANI_BOT",),
-         InlineKeyboardButton(text="• sꭎ֟፝᷍ᴘᴘσʀᴛ •", url=f"https://t.me/+1iBW9rtUxfszMzY1",)
-        ],
-        [
-         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
-        ],
-        ]
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
     return buttons
 
 
 def stream_markup(_, chat_id):
+    buttons = [
         [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/HIMANSHI_MUSIC_BOT?startgroup=true",)
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-         InlineKeyboardButton(text="• ꭎ֟፝᷍ᴘᴅʌᴛɛ •", url=f"https://t.me/ALL_SANATANI_BOT",),
-         InlineKeyboardButton(text="• sꭎ֟፝᷍ᴘᴘσʀᴛ •", url=f"https://t.me/+1iBW9rtUxfszMzY1",)],
-        [
-         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-        ]
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
     return buttons
 
 
